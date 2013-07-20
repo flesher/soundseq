d90282379682d025c95b5d59bb5c5b3d3827d006
Matt Flesher <matt.flesher@gmail.com>
2013-07-20 17:13:01 -0600

    recursive timeout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var phase     = 0;
+var phase  = 0;
 var seq    = 0;
 var sequence = [  
   [0,1,2,3,4,5,6,7],
@@ -8,7 +8,8 @@ var sequence = [
   [0,0,0,0,0,0,0,0],
   [1,1,1,1,1,1,1,1],
   [2,2,2,2,2,2,2,2]  
-]
+];
+var go;
 
 //
 // starting point for the application
@@ -20,36 +21,25 @@ track1.ready(function() {
   console.log('track ready', track1.profile, track1.analysis);
 
   var BPM          = track1.tempo();
-  console.log(BPM);
   var beatInterval = 1000 / (BPM/60);
+  var beatInfo     = [];
+  
+  console.log(beatInfo);
 
-    var sound = new Howl({
-      urls: [track1.file],
-      sprite: {
-        one   : track1.beat(1, 0),
-        two   : track1.beat(1, 1),
-        three : track1.beat(1, 2),
-        four  : track1.beat(1, 3),
-        five  : track1.beat(1, 4),
-        six   : track1.beat(1, 5),
-        seven : track1.beat(1, 6),
-        eight : track1.beat(1, 7)
-      }
-    });
+  var sound = new Howl({
+    urls: [track1.file],
+    sprite: {
+      one   : track1.beat(1,0),
+      two   : track1.beat(1,1),
+      three : track1.beat(1,2),
+      four  : track1.beat(1,3),
+      five  : track1.beat(1,4),
+      six   : track1.beat(1,5),
+      seven : track1.beat(1,6),
+      eight : track1.beat(1,7)
+    }
+  });
 
-  // var sound = new Howl({
-  //   urls: [track1.file],
-  //   sprite: {
-  //     one   : [0, beatInterval * 2],
-  //     two   : [beatInterval * 2, beatInterval * 2],
-  //     three : [beatInterval * 4, beatInterval * 2],
-  //     four  : [beatInterval * 6, beatInterval * 2],
-  //     five  : [beatInterval * 8, beatInterval * 2],
-  //     six   : [beatInterval * 10, beatInterval * 2],
-  //     seven : [beatInterval * 12, beatInterval * 2],
-  //     eight : [beatInterval * 14, beatInterval * 2]
-  //   }
-  // });
 
   $('button').click(function(){
     seq += 1;
@@ -58,18 +48,29 @@ track1.ready(function() {
     if (seq > sequence.length) seq = 0;
   });
 
-  setInterval( function() {
+  function playSlice() {
     var num = Math.floor(Math.random() * 8);
     var sprite = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
     var sliceToPlay;
 
     sliceToPlay = sequence[seq][phase];
 
+    console.log(sliceToPlay + ' ' + phase);
+
     sound.play(sprite[sliceToPlay]);
+
+    loop();
 
     phase += 1;
     if (phase > 7) phase = 0;
+  }
 
-  }, beatInterval * 2);
+  loop = function() {
+    setTimeout(function(){
+      playSlice();
+    }, track1.beat(1, phase)[1] );
+  }
+
+  playSlice();
 });
 
