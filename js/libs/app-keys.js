0fc1b787307690d5e05e2f6491e877eb057708e4
Matt Flesher <matt.flesher@gmail.com>
2013-07-20 14:45:57 -0600

    howler sequencing

---
+++ js/libs/app-keys.js
@@ -0,0 +1,3 @@
+var echonestKey = 'HVEUE8UW1SPXMNW3R'
+var consumerKey = 'eeb88120eeb26e52f0faad9fd3506e78' 
+var secret =  'wDWudbjeR5usKbku+qwvkg'
---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function(){
+
+  var BPM          = 120;
+  var beatInterval = 1000 / (BPM/60);
+
+  var sound = new Howl({
+    urls: ['music/pl-loop.mp3'],
+    sprite: {
+      one   : [0, beatInterval * 2],
+      two   : [beatInterval * 2, beatInterval * 2],
+      three : [beatInterval * 4, beatInterval * 2],
+      four  : [beatInterval * 6, beatInterval * 2],
+      five  : [beatInterval * 8, beatInterval * 2],
+      six   : [beatInterval * 10, beatInterval * 2],
+      seven : [beatInterval * 12, beatInterval * 2],
+      eight : [beatInterval * 14, beatInterval * 2]
+    }
+  });
+
+  setInterval(function(){
+    var num = Math.floor(Math.random() * 8);
+    var sprite = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+    console.log(num);
+    sound.play(sprite[num]);
+  }, beatInterval * 2);
+
+})();
