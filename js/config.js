7e807cfb06ce83dcb22766eeb853807485f9d79e
cavis <ryancavis@gmail.com>
2013-07-20 20:23:50 -0600

    more filez; try really hard to get at least 8 sections per track

--- js/config.js
+++ js/config.js
@@ -6,7 +6,81 @@ CONFIG = {
   TRACKS: [
     {
       track_id: 'TRHEAIX13FFDA5F848',
-      file:     'music/2-01 Reel 15 Break 5.mp3'
-    }
+      file:     'music/2-01 Reel 15 Break 5.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 15 Break 5'
+    },
+    {
+      track_id: 'TRWGSLJ13FFEF16087',
+      file:     'music/2-02 Reel 5 Break 3.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 5 Break 3'
+    },
+    {
+      track_id: 'TRAYQCS13FFEF2D077',
+      file:     'music/2-03 Reel 9 Break 6.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 9 Break 6'
+    },
+    {
+      track_id: 'TRBTRXH13FFEF3AF70',
+      file:     'music/2-04 Reel 11 Break 2.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 11 Break 2'
+    },
+    {
+      track_id: 'TRSBNGO13FFEF5751A',
+      file:     'music/2-05 Reel 6 Break 4.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 6 Break 4'
+    },
+    {
+      track_id: 'TRTNCPI13FFEF66357',
+      file:     'music/2-06 Reel 8 Break 2.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 8 Break 2'
+    },
+    {
+      track_id: 'TRWGSYX13FFEF77CFC',
+      file:     'music/2-07 Reel 18 Session 1.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 18 Session 1'
+    },
+    {
+      track_id: 'TRXISXD13FFEF86E1C',
+      file:     'music/2-08 Reel 17 Break 4.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 17 Break 4'
+    },
+    {
+      track_id: 'TRBZLRH13FFEF952DC',
+      file:     'music/2-09 Reel 4 Break 3.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 4 Break 3'
+    },
+    {
+      track_id: 'TRSBELX13FFEFA0C12',
+      file:     'music/2-10 Reel 12 Break 2.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 12 Break 2'
+    },
+    {
+      track_id: 'TRWWZBL13FFEFAECC1',
+      file:     'music/2-11 Reel 6 Break 5.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 6 Break 5'
+    },
+    {
+      track_id: 'TRFMTHG13FFEFBB2D5',
+      file:     'music/2-12 Reel 3 Break 3.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 3 Break 3'
+    },
+    {
+      track_id: 'TRFEDOS13FFEFC8CF7',
+      file:     'music/2-13 Reel 7 Break 1.mp3',
+      artist:   'Pretty Lights',
+      title:    'Reel 7 Break 1'
+    },
   ]
 };
