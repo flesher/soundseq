7692d8167b9f81b413f5a87bfbc8b949b7a363a7
Cory Shaw <coryshaw@gmail.com>
2013-07-21 12:57:38 -0600

    keyboard shortcuts up the wazoo

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,179 @@ $(document).ready(function(){
 
   var windowHeight, navHeight, footerHeight, partsHeight, partHeight;
 
+  
+  //keyboard shortcuts
+
+
+  // part shortcuts
+
+  $(document).bind('keypress', 'space',function (evt){
+    playpause();
+  });
+
+  $(document).bind('keypress', 'a',function (evt){
+    if (sequencer.track){
+      sequencer.replace(0);
+      $('#parts .part[data-part="0"]').addClass('queued');
+    }
+  });
+
+  $(document).bind('keypress', 's',function (evt){
+    if (sequencer.track){
+      sequencer.replace(1);
+      $('#parts .part[data-part="1"]').addClass('queued');
+    }
+  });
+
+  $(document).bind('keypress', 'd',function (evt){
+    if (sequencer.track){
+      sequencer.replace(2);
+      $('#parts .part[data-part="2"]').addClass('queued');
+    }
+  });
+
+  $(document).bind('keypress', 'f',function (evt){
+    if (sequencer.track){
+      sequencer.replace(3);
+      $('#parts .part[data-part="3"]').addClass('queued');
+    }
+  });
+
+  $(document).bind('keypress', 'g',function (evt){
+    if (sequencer.track){
+      sequencer.replace(4);
+      $('#parts .part[data-part="4"]').addClass('queued');
+    }
+  });
+
+  $(document).bind('keypress', 'h',function (evt){
+    if (sequencer.track){
+      sequencer.replace(5);
+      $('#parts .part[data-part="5"]').addClass('queued');
+    }
+  });
+
+  $(document).bind('keypress', 'j',function (evt){
+    if (sequencer.track){
+      sequencer.replace(6);
+      $('#parts .part[data-part="6"]').addClass('queued');
+    }
+  });
+
+  $(document).bind('keypress', 'k',function (evt){
+    if (sequencer.track){
+      sequencer.replace(7);
+      $('#parts .part[data-part="7"]').addClass('queued');
+    }
+  });
+
+  
+  // section shortcuts
+
+  $(document).bind('keypress', 'z',function (evt){
+    if (sequencer.track){
+      sequencer.section(0);
+      $('#sections .section[data-section="0"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+  $(document).bind('keypress', 'x',function (evt){
+    if (sequencer.track){
+      sequencer.section(1);
+      $('#sections .section[data-section="1"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+  $(document).bind('keypress', 'c',function (evt){
+    if (sequencer.track){
+      sequencer.section(2);
+      $('#sections .section[data-section="2"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+  $(document).bind('keypress', 'v',function (evt){
+    if (sequencer.track){
+      sequencer.section(3);
+      $('#sections .section[data-section="3"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+  $(document).bind('keypress', 'b',function (evt){
+    if (sequencer.track){
+      sequencer.section(4);
+      $('#sections .section[data-section="4"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+  $(document).bind('keypress', 'n',function (evt){
+    if (sequencer.track){
+      sequencer.section(5);
+      $('#sections .section[data-section="5"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+  $(document).bind('keypress', 'm',function (evt){
+    if (sequencer.track){
+      sequencer.section(6);
+      $('#sections .section[data-section="6"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+  $(document).bind('keypress', '<',function (evt){
+    if (sequencer.track){
+      sequencer.section(7);
+      $('#sections .section[data-section="7"]').addClass('queued');
+      $('#sections .current, #sections .section').removeClass('current');
+    }
+  });
+
+
+  // utility shortcuts
+
+  $(document).bind('keypress', 'r',function (evt){
+    if (sequencer.track){
+      $('#record').toggleClass('recording');
+    }
+  });
+
+  $(document).bind('keypress', '1',function (evt){
+    if (sequencer.track){
+      $('.sequence .part').removeClass('current');
+      $('#seq1, #seq2, #seq3').removeClass('current');
+      $('#seq1').addClass('current');
+      sequencer.sequence(0);
+    }
+  });
+
+  $(document).bind('keypress', '2',function (evt){
+    if (sequencer.track){
+      $('.sequence .part').removeClass('current');
+      $('#seq1, #seq2, #seq3').removeClass('current');
+      $('#seq2').addClass('current');
+      sequencer.sequence(1);
+    }
+  });
+
+  $(document).bind('keypress', '3',function (evt){
+    if (sequencer.track){
+      $('.sequence .part').removeClass('current');
+      $('#seq1, #seq2, #seq3').removeClass('current');
+      $('#seq3').addClass('current');
+      sequencer.sequence(2);
+    }
+  });
+
+
+
+  // Init UI
+
   function initUI(){
     windowHeight = $(window).height();
     windowWidth = $(window).width();
@@ -20,9 +193,13 @@ $(document).ready(function(){
     initUI();
   });
 
+  function playpause(){
+    $('#playpause').hasClass('paused') ? sequencer.play() : sequencer.pause();
+    $('#playpause').toggleClass('paused');
+  }
+
   $('#playpause').on('tap', function(){
-    $(this).hasClass('paused') ? sequencer.play() : sequencer.pause();
-    $(this).toggleClass('paused');
+    playpause();
   });
 
   $('#touch, #gyro').on('tap', function(){
