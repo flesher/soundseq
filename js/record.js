da2c3ccf0d19638ba8d7dd754ac4a5f3cb92d8ac
Matt Flesher <matt.flesher@gmail.com>
2013-07-21 11:39:52 -0600

    add

---
+++ js/record.js
@@ -0,0 +1,32 @@
+var armed      = false;
+var rec        = new Recorder(sequencer.howl._audioNode[0]);
+
+function saveWav(blob){
+  var soundFile = blob;
+  return soundFile;
+  console.log(soundFile);
+}
+
+
+// //set key event when key is pressed
+// onKeyDown = function(evt) {
+//   console.log('down');
+
+//   if (evt.keyCode == 82) {
+//     evt.preventDefault(); 
+//     if (!rec) {
+//       rec.record(); 
+//       armed = true;
+//       console.log('begin');
+//     }
+//     else {
+//       console.log('end');
+//       rec.stop();
+//       rec.exportWAV(saveWav);
+//       armed = false;
+
+//     } 
+//   }
+// }
+
+$(document).keydown(onKeyDown);
