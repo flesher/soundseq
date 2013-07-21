d10e50e835ae503605a3fd195c89945fe4c355fe
cavis <ryancavis@gmail.com>
2013-07-20 22:32:44 -0600

    try some different sequence strategies

--- js/track.js
+++ js/track.js
@@ -1,5 +1,6 @@
 // config
-var CONFIDENCE_THRESHOLD = 0.3;
+var BAR_CONFIDENCE_THRESHOLD = 0.3;
+var BEAT_CONFIDENCE_THRESHOLD = 0.8;
 var BEATS_PER_SECTION = 8;
 var MINIMUM_SECTIONS = 8;
 
@@ -44,16 +45,7 @@ function Track(index) {
         },
         success: function(data, textStatus, jqXHR) {
           this.analysis = data;
-
-          // try to get at least 8 sections
-          var threshold = CONFIDENCE_THRESHOLD;
-          this.init(threshold);
-          var attempts = 1;
-          while (this.sections.length < MINIMUM_SECTIONS && attempts < 4) {
-            threshold -= 0.1;
-            this.init(threshold);
-            attempts++;
-          }
+          this.init();
 
           // execute any ready callbacks
           _.each(this.readyFn, function(cb) { cb(); });
@@ -67,41 +59,9 @@ function Track(index) {
 //
 // what data is usable, and format it into something we can use
 //
-Track.prototype.init = function(threshold) {
-  var analysis = this.analysis;
-
-  // don't run the entire beats array every time
-  var beatIndex = 0;
-
-  // 1) find usable sections
-  var sections = [];
-  _.each(analysis.bars, function(bar, idx) {
-    if (bar.confidence >= threshold) {
-      var newSection = {
-        start: bar.start,
-        startBar: idx,
-        beats: []
-      }
-
-      // find some beats in this section
-      for (beatIndex; beatIndex <= analysis.beats.length; beatIndex++) {
-        var beat = analysis.beats[beatIndex];
-        if (beat.start >= newSection.start) {
-          newSection.beats = analysis.beats.slice(beatIndex, beatIndex + BEATS_PER_SECTION);
-          break;
-        }
-      }
-
-      // must have minimum beats to actually be a section
-      if (newSection.beats.length == BEATS_PER_SECTION) {
-        sections.push(newSection);
-      }
-    }
-  });
-
-  // assign it
-  this.sections = sections;
-  console.log('found ' + sections.length + ' sections with confidence > ' + threshold);
+Track.prototype.init = function() {
+  this.sections = STRATEGY.all(this.analysis, BEATS_PER_SECTION);
+  console.log('found ' + this.sections.length + ' sections');
 }
 
 
