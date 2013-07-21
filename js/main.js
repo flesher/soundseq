8754458b0c2c8f40ee502166334c37fcb556f8bb
cavis <ryancavis@gmail.com>
2013-07-20 18:22:23 -0600

    abstrict sequencer

--- js/main.js
+++ js/main.js
@@ -1,75 +1,19 @@
-var phase  = 0;
-var seq    = 0;
-var sequence = [  
-  [0,1,2,3,4,5,6,7],
-  [0,0,2,2,3,3,4,4],
-  [7,6,5,4,3,2,1,0],
-  [0,0,0,0,0,0,0,0],
-  [1,1,1,1,1,1,1,1],
-  [2,2,2,2,2,2,2,2]  
-];
-var go;
-
 //
 // starting point for the application
 //
 var track1 = new Track(0);
+var sequencer = new Sequencer(track1);
 
-track1.ready(function() {
+$(function() {
 
-  console.log('track ready', track1.profile, track1.analysis);
-
-  var BPM          = track1.tempo();
-  var beatInterval = 1000 / (BPM/60);
-  var beatInfo     = [];
-  
-  console.log(beatInfo);
-
-  var sound = new Howl({
-    urls: [track1.file],
-    sprite: {
-      one   : track1.beat(1,0),
-      two   : track1.beat(1,1),
-      three : track1.beat(1,2),
-      four  : track1.beat(1,3),
-      five  : track1.beat(1,4),
-      six   : track1.beat(1,5),
-      seven : track1.beat(1,6),
-      eight : track1.beat(1,7)
-    }
+  // change events
+  sequencer.on('beat', function(phase, beat) {
+    console.log('beat', phase, beat);
   });
 
-  $('button').click(function(){
-    seq += 1;
-    console.log(seq);
-    $(this).html(seq);
-    if (seq > sequence.length) seq = 0;
+  // play button
+  $('#playpause').click(function() {
+    $(this).hasClass('paused') ? sequencer.play() : sequencer.pause();
   });
 
-  function playSlice() {
-    var num = Math.floor(Math.random() * 8);
-    var sprite = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-    var sliceToPlay;
-
-    sliceToPlay = sequence[seq][phase];
-
-    $('#parts div').removeClass('current');
-    $('#parts div[data-part="'+num+'"]').addClass('current');
-
-    console.log(sliceToPlay + ' ' + phase);
-
-    sound.play(sprite[sliceToPlay]);
-    loop();
-    phase += 1;
-    if (phase > 7) phase = 0;
-  }
-
-  loop = function() {
-    setTimeout(function(){
-      playSlice();
-    }, track1.beat(1, phase)[1] );
-  }
-
-  playSlice();
 });
-
---
+++ js/sequencer.js
@@ -0,0 +1,122 @@
+//
+// the sequencer
+//
+function Sequencer(track) {
+  self = this;
+
+  // properties
+  this.phase    = 0;
+  this.track    = track;
+  this.handlers = {};
+
+  // init playback
+  this.howl = new Howl({
+    urls: [track.file],
+    autoplay: false,
+    onload: function() {
+      self._loaded = true;
+      if (self.playing) self._playLoop();
+    }
+  });
+
+  // init sequence/section
+  track.ready(function() {
+    self.section(0);
+    self.sequence(0);
+    if (self.playing) self._playLoop();
+  });
+
+}
+
+//
+// private methods
+//
+
+// the main play loop
+Sequencer.prototype._playLoop = function() {
+  if (this.playing && this._sequence && this._loaded) {
+    var beatNames = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+    var beatIdx   = this._sequence[this.phase];
+    var beatName  = beatNames[beatIdx];
+    var beatDur   = this._sectionBeats[beatName][1];
+
+    // play the sound
+    this.howl.play(beatName);
+    this.fire('beat', this.phase, beatIdx);
+
+    // increment phase and delay
+    this.phase = (this.phase + 1) % 8;
+    _.delay(_.bind(this._playLoop, this), beatDur);
+  }
+}
+
+//
+// public methods
+//
+
+// event handling (single - TODO multiple)
+Sequencer.prototype.on = function(event, callback, scope) {
+  this.handlers[event] = _.bind(callback, scope);
+}
+Sequencer.prototype.off = function(event, callback) {
+  delete this.handlers[event];
+}
+Sequencer.prototype.fire = function(event) {
+  if (this.handlers[event]) this.handlers[event].apply(this, _.rest(arguments))
+}
+
+// get/set the current section
+Sequencer.prototype.section = function(idx) {
+  if (idx === undefined) {
+    return this._sectionIdx;
+  }
+  else if (idx != this._sectionIdx) {
+    this._sectionIdx = idx;
+    this._sectionBeats = {
+      one:   this.track.beat(idx, 0),
+      two:   this.track.beat(idx, 1),
+      three: this.track.beat(idx, 2),
+      four:  this.track.beat(idx, 3),
+      five:  this.track.beat(idx, 4),
+      six:   this.track.beat(idx, 5),
+      seven: this.track.beat(idx, 6),
+      eight: this.track.beat(idx, 7)
+    };
+    this.howl.sprite(this._sectionBeats);
+  }
+}
+
+// get/set the current sequence
+Sequencer.prototype.sequence = function(idx) {
+  if (idx === undefined) {
+    return this._sequenceIdx;
+  }
+  else if (idx != this._sequenceIdx) {
+    this._sequenceIdx = idx;
+    var sequences = [
+      [0,1,2,3,4,5,6,7],
+      [0,0,2,2,3,3,4,4],
+      [7,6,5,4,3,2,1,0],
+      [0,0,0,0,0,0,0,0],
+      [1,1,1,1,1,1,1,1],
+      [2,2,2,2,2,2,2,2]
+    ];
+    this._sequence = sequences[idx];
+  }
+}
+
+// start playback
+Sequencer.prototype.play = function() {
+  if (!this.playing) {
+    this.playing = true;
+    this._playLoop();
+  }
+}
+
+// pause playback
+Sequencer.prototype.pause = function() {
+  if (this.playing) {
+    this.playing = false;
+    this.howl.stop();
+  }
+}
