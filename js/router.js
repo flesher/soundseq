a805f8a2dddf05f8b5cc90b87e174c2f35f2ae07
cavis <ryancavis@gmail.com>
2013-07-21 13:00:17 -0600

    add uploader back in

--- js/router.js
+++ js/router.js
@@ -14,20 +14,22 @@
 
   var app_router = new AppRouter;
 
+  // everything's loaded
+  $('#preloader').on('loaded', function(){
+    $('#intro').fadeOut(500);
+  });
+
+  // hide intro when we go sequencer
   app_router.on('route:sequencer', function() {
     $('#intro').fadeOut();
-
-    $('#preloader').on('loaded', function(){
-      $(this).fadeOut(500);
-    });
   });
 
   app_router.on('route:defaultRoute', function() {
-    $("#intro").html('');
+    $("#intro .tracks").html('');
 
     // show the configured tracks
     _.each(CONFIG.TRACKS, function(track, idx) {
-      $('#intro').append(
+      $('#intro .tracks').append(
         '<a href="#sequencer" class="track-pick" data-track="' + idx + '">' +
         track.artist + ' - ' + track.title + '</a>');
     });
