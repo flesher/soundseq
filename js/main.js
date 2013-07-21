e4f6fbe64d4b9a92835d5321fe89a88ed9dc7433
Cory Shaw <coryshaw@gmail.com>
2013-07-20 20:33:20 -0600

    fixed conflicts

--- js/main.js
+++ js/main.js
@@ -8,22 +8,20 @@ $(function() {
 
   // change events
   sequencer.on('beat', function(phase, beat) {
-    $('#parts .part').removeClass('current');
+    $('#parts .part, #sections .section .part').removeClass('current');
     $('#parts .part[data-part="'+beat+'"]').addClass('current');
+    $('#parts .part[data-part="'+beat+'"],#sections .section.current .part[data-part="'+beat+'"]').addClass('current');
   });
 
   // play button
   $('#playpause').on('tap', function(){
     $(this).hasClass('paused') ? sequencer.play() : sequencer.pause();
-
-    console.log('beat', phase, beat);
     $('#parts .part, #sections .section.current .part').removeClass('current');
-    $('#parts .part[data-part="'+beat+'"],#sections .section.current .part[data-part="'+beat+'"]').addClass('current');
     $('#parts .part, #sections .section').removeClass('queued');
   });
 
   // section switcher
-  $('#sections > div').on('tap', function() {
+  $('#sections > .section').on('tap', function() {
     if (!$(this).hasClass('current')) {
       $('#sections .current').removeClass('current');
       $(this).addClass('current');
